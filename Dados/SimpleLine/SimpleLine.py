4dd45d395eb12a9c85dc7a3a3592b7ba295ddad8
On0n0k1 <stiltztinkerstein@gmail.com>
2020-11-09 09:54:50 -0300

    Adding function annotations, fixing docstrings and improving readability.

--- Dados/SimpleLine/SimpleLine.py
+++ Dados/SimpleLine/SimpleLine.py
@@ -26,30 +26,62 @@ __maintainer__ = "Lucas Alessandro do Carmo Lemos"
 __email__ = "stiltztinkerstein@gmail.com"
 __status__ = (["Prototype", "Development", "Production"])[2]
 
+from typing import Union
+
 
 class SimpleLine:
     """ A single line of the text file.
 
-        Used in Script_Info. Also used when reading the file."""
+    Used in Script_Info. Also used when reading the file.
 
-    # Position in the text file.
-    posicao = 0
+    Used for treating strings formatted as f'{tipo}: {texto}'
 
-    # Argument name. If tipo == f'null', the line will be treated as a comment line.
-    tipo = ""
+    If read string is not in that format, {tipo} will be None, and {texto} will be the whole line.
 
-    # Argument value.
-    texto = ""
+    'posicao' is an optional attribute that could be used for tracking it's index in a list, for instance.
 
-    def __init__(self, texto=None, posicao=None, speaking=False):
+    Methods:
+
+        processarlinha: read a line and store the value
+
+        __repr__: this object as a formatted string. Used for saving.
+
+        __init__: construct this object with no args, another SimpleLine or a string to read.
+
+        gettipo: returns tipo as string or None.
+
+        gettexto: returns texto as string.
+
+        getposicao: returns posicao as integer.
+
+        settipo: set tipo with a string or None.
+
+        settexto: set texto with a string.
+
+        setposicao: set posicao with an integer.
+    """
+
+    def __init__(self, texto: Union[None, 'SimpleLine', str] = None, posicao: Union[None, int] = None, speaking: bool
+                 = False):
         """ Starts the object with a line from the text file. Formatted as f'{tipo}: {texto}'.
 
-        :param texto: String. The line loaded from the file. If it is undeclared. This object will be f'{tipo}: {texto}'
-            and position = 0.
-        :param posicao: Integer. Position of the line. Used when saving the file. If it is undeclared, posicao = 0.
-        :param speaking: Boolean. Used for debugging. Tells processarLinha to print what it is reading."""
+        :param texto: String or SimpleLine. The line loaded from the file. If it is undeclared. This object will be
+        f'{tipo}: {texto}' and position = 0.
 
-        # print(f"texto: ({texto})  posicao: ({posicao})")
+        :param posicao: Integer. Position of the line. Used when saving the file. If it is undeclared, posicao = 0.
+
+        :param speaking: Boolean. Used for debugging. Tells processarLinha to print what it is reading.
+        """
+
+        if (isinstance(texto, SimpleLine) or isinstance(texto, str) or (texto is None)) is False:
+            raise TypeError(f"Invalid type for texto: {texto}({type(texto)})")
+        if ((posicao is None) or isinstance(posicao, int)) is False:
+            raise TypeError(f"Invalid type for posicao: {posicao}({type(posicao)})")
+        if isinstance(speaking, bool) is False:
+            raise TypeError(f"Invalid type for speaking: {speaking}({type(speaking)})")
+
+        self.posicao, self.tipo, self.texto = 0, None, ""
+
         self.__defaulttitles__ = ("[Script Info]", "[V4+ Styles]", "[Events]")
         self.__lowertitles__ = (_.lower() for _ in self.__defaulttitles__)
         if texto is None:
@@ -67,36 +99,42 @@ class SimpleLine:
                 self.setposicao(0)
                 self.processarlinha(f"{texto}")
             else:
-                if isinstance(posicao, int):
+                if isinstance(posicao, int) is False:
                     TypeError(f"{posicao} must be int or omitted")
                 self.setposicao(posicao)
                 self.processarlinha(f"{texto}", speaking)
 
-    def __repr__(self):
-        """Prints the line in the same format it can be found in a SSA file. Used when saving.
+    def __repr__(self) -> str:
+        """Prints the line in the same format it can be found in a SSA file.
 
-        :return: if tipo=='null', return f"{tipo}: {texto}"
-            else f"{texto}" """
+        Used when saving. Can be called with f'{NAME!r}'
+
+        :return: if tipo is not None (therefore comment), return f"{tipo}: {texto}" else f"{texto}"
+        """
 
         if self.tipo is None:
             return f"{self.texto}"
         else:
             return f"{self.tipo}: {self.texto}"
 
-    def processarlinha(self, texto, speaking=False):
+    def processarlinha(self, texto: str, speaking: bool = False) -> 'SimpleLine':
         """ Read a line and assign it's values to this object. Called by the constructor. Position isn't set here.
 
         :param texto: string. The line to be read. Formatted as f'{tipo}: {texto}'
         :param speaking: Boolean. Debugging parameter. The method prints what it is reading in the output.
-        :return: None.
+        :return: self.
 
         comments are SimpleLine with tipo = 'null' values.
+
         tipo is None when:
             -starts with ';'
+
             -starts with '!:'
+
             -starts with 'comment:'
 
-        If speaking == true. Print f"[{self.getPosicao()}]{self.getTipo()}: {self.getTexto()}" """
+        If speaking == true, Print f"[{self.getPosicao()}]{self.getTipo()}: {self.getTexto()}"
+        """
 
         if isinstance(texto, str) is False:
             raise TypeError(f"{texto} must be a string value.")
@@ -112,6 +150,7 @@ class SimpleLine:
                 self.settexto(self.__defaulttitles__[checking.index(True)])
             else:
                 chop = texto.split(":", 1)
+                # Strip() will clear out every empty space at the start/end of each string
                 chop = [_.strip() for _ in chop]
                 if(len(chop) == 1) or (chop[0] == "!") or (chop[0].lower() == "comment"):
                     self.settipo(None)
@@ -119,65 +158,76 @@ class SimpleLine:
                 else:
                     # Sets the first index from list to tipo from saida,
                     # Sets the second index from list to texto from saida
-                    # Strip() will clear out every empty space at the start/end of each string
-
                     self.settipo(chop[0])
                     self.settexto(chop[1].strip())
 
         if speaking:
             # For Debugging. Prints what it read.
             print(f"[{self.getposicao()}]{self.gettipo()}: {self.gettexto()}")
+        return self
 
-    def gettipo(self):
+    def gettipo(self) -> str:
         """ Return tipo. From the format f'{tipo}: {texto}'
 
-            :return: String tipo"""
+        :return: String tipo
+        """
+
         return self.tipo
 
-    def gettexto(self):
+    def gettexto(self) -> str:
         """ Return texto. From the format f'{tipo}: {texto}'
 
-        :return: String texto"""
+        :return: String texto
+        """
+
         return self.texto
 
-    def getposicao(self):
+    def getposicao(self) -> int:
         """ Return posicao. The position of this line in the text file.
 
-        :return: Integer posicao"""
+        :return: Integer posicao
+        """
+
         return self.posicao
 
-    def setposicao(self, posicao):
+    def setposicao(self, posicao: int) -> 'SimpleLine':
         """ Set posicao. The position of this line in the text file
 
         :param posicao: Integer
         :return: self
         """
+
         if isinstance(posicao, int) is False:
             raise TypeError(f"{posicao} must be an integer")
 
         self.posicao = posicao
         return self
 
-    def settipo(self, tipo):
-        """ Set tipo. From the format f'{tipo}: {texto}'.
+    def settipo(self, tipo: Union[str, None]) -> 'SimpleLine':
+        """ Set tipo.
+
+        From the format f'{tipo}: {texto}'.
 
         :param tipo: String
-        :return: self"""
+        :return: self
+        """
+
         if tipo is None:
             self.tipo = None
             return self
-
         if isinstance(tipo, str) is False:
             raise TypeError(f"{tipo} must be a string or None")
 
         self.tipo = tipo
         return self
     
-    def settexto(self, texto):
+    def settexto(self, texto: str) -> 'SimpleLine':
         """ Set Texto. From the format f'{tipo}: {texto}'.
 
         :param texto: String
-        :return: self"""
+        :return: self
+        """
+
         if isinstance(texto, str) is False:
             raise TypeError(f"{texto} must be a string")
         self.texto = texto
@@ -186,8 +236,6 @@ class SimpleLine:
 
 # debugging
 if __name__ == "__main__":
-    # I used a subtitle file generated by the website aegisub, which is 100x better than myne. Please consider
-    # supporting them. I'm only making this project to add to my curriculum. No intention of finishing
     x = [
             "[script info]",
             "; Script generated by Aegisub 2.1.9",
